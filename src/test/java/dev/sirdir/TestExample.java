b7a968a83d6cbf0b42d0cba79cd29e06cd2221c4
SirDiR <phddir@gmail.com>
2019-12-22 11:57:55 +0100

    initial commit

---
+++ src/test/java/dev/sirdir/TestExample.java
@@ -0,0 +1,37 @@
+package dev.sirdir;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class TestExample {
+
+    @ParameterizedTest
+    @ValueSource(strings = "Hello world")
+    void helloWorld(String textExpected) {
+        String helloWorldActual = "Hello world";
+        assertEquals(textExpected, helloWorldActual);
+    }
+
+    @Test
+    void assumeFails() {
+        assumeTrue(false);
+        assumeTrue(true);
+    }
+
+    @Test
+    void assumePass() {
+        assumeTrue(true);
+        assert 1 > 2 : "some old fashion java asssertion fail";
+    }
+
+    @Test
+    @Disabled
+    void someDisabledTestToSeeItInReport() {
+        //ignored
+    }
+}
